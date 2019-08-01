eb4742f4053e28f152b84d52a6f5d08494c4f0f7
martin gardner II <martingardnerii@gmail.com>
2019-08-01 13:36:20 -0500

    base state logic for one checkbox to control sibling checkboxes

--- components/roomBox.js
+++ components/roomBox.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Adultpop from "./adultpop";
 import Childpop from "./childpop";
 
 const Roombox = props => {
-  //console.log("Roombox", data);
-  const [getCheckbox, setCheckbox] = useState(false);
+  console.log("Roombox", props);
+  const [getCheckbox, setCheckbox] = useState(props.params.checkboxState);
 
   const updateCheckbox = e => {
     console.log("updateCheckbox", getCheckbox, !getCheckbox);
     setCheckbox(!getCheckbox);
+    props.checkNeighborRoom(props.params.order, !getCheckbox);
   };
+
+  useEffect(() => {
+    setCheckbox(props.params.checkboxState);
+  }, [props.params.checkboxState]);
 
   return (
     <div>
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,55 @@
 import React, { useState } from "react";
 import Roombox from "../components/roomBox";
 
-export const roomData = [
-  { name: "Room 1", checkbox: false },
-  { name: "Room 2", checkbox: true },
-  { name: "Room 3", checkbox: true },
-  { name: "Room 4", checkbox: true }
-];
-
 const Main = () => {
+  const [getRoom1, setRoom1] = useState(true);
+  const [getRoom2, setRoom2] = useState(false);
+  const [getRoom3, setRoom3] = useState(false);
+  const [getRoom4, setRoom4] = useState(false);
+
   const formSubmit = e => {
     e.preventDefault();
   };
 
+  const checkNeighborRoom = (roomOrder, checked) => {
+    console.log("checkNeighborRoom", roomOrder, checked);
+    switch (roomOrder) {
+      case 4:
+        console.log("4");
+        setRoom4(checked);
+      case 3:
+        console.log("3");
+        setRoom3(checked);
+      case 2:
+        console.log("2");
+        setRoom2(checked);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const roomData = [
+    { name: "Room 1", checkbox: false, order: 1, checkboxState: getRoom1 },
+    { name: "Room 2", checkbox: true, order: 2, checkboxState: getRoom2 },
+    { name: "Room 3", checkbox: true, order: 3, checkboxState: getRoom3 },
+    { name: "Room 4", checkbox: true, order: 4, checkboxState: getRoom4 }
+  ];
+
+  console.log("index.js", getRoom1, getRoom2, getRoom3, getRoom4);
   return (
     <>
       <form onSubmit={formSubmit}>
         <fieldset>
           {roomData &&
             roomData.map((data, index) => {
-              return <Roombox key={index + data.name} params={data} />;
+              return (
+                <Roombox
+                  key={index + data.name}
+                  params={data}
+                  checkNeighborRoom={checkNeighborRoom}
+                />
+              );
             })}
         </fieldset>
         <button>Submit</button>
