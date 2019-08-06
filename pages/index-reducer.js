ce42d53490cf28a9c53688f1028541fddefbd342
martin gardner II <martingardnerii@gmail.com>
2019-08-05 23:13:41 -0500

    updated name of localStorage item so there would be no conflicts between index and index-reducer page

--- pages/index-reducer.js
+++ pages/index-reducer.js
@@ -6,77 +6,40 @@ import RoomboxReducer from "../components/roomBox-reducer";
 
 const MainReducer = () => {
   const [dataReducer, dispatch] = useReducer(RoomsReducer, []);
-  /*
-  const [getRoom1, setRoom1] = useState(true);
-  const [getAdultPop1, setAdultPop1] = useState(1);
-  const [getChildPop1, setChildPop1] = useState(0);
-  const [getRoom2, setRoom2] = useState(false);
-  const [getAdultPop2, setAdultPop2] = useState(1);
-  const [getChildPop2, setChildPop2] = useState(0);
-  const [getRoom3, setRoom3] = useState(false);
-  const [getAdultPop3, setAdultPop3] = useState(1);
-  const [getChildPop3, setChildPop3] = useState(0);
-  const [getRoom4, setRoom4] = useState(false);
-  const [getAdultPop4, setAdultPop4] = useState(1);
-  const [getChildPop4, setChildPop4] = useState(0);
-*/
+
   const formSubmit = e => {
     e.preventDefault();
     let form = document.querySelector("form");
-    //localStorageSet();
+    localStorageSet();
   };
 
-  /*
   const localStorageSet = () => {
     localStorage.clear();
-
-    let localStorageObj = {
-      AdultPop1: getAdultPop1,
-      ChildPop1: getChildPop1,
-      Room2: getRoom2,
-      AdultPop2: getAdultPop2,
-      ChildPop2: getChildPop2,
-      Room3: getRoom3,
-      AdultPop3: getAdultPop3,
-      ChildPop3: getChildPop3,
-      Room4: getRoom4,
-      AdultPop4: getAdultPop4,
-      ChildPop4: getChildPop4
-    };
-
-    localStorage.setItem("Rooms", JSON.stringify(localStorageObj));
+    localStorage.setItem("RoomsReducer", JSON.stringify(dataReducer));
   };
 
   const setStateFromStorage = () => {
-    let storage = JSON.parse(localStorage.getItem("Rooms"));
-
-    setAdultPop1(storage.AdultPop1);
-    setChildPop1(storage.ChildPop1);
-    setRoom2(storage.Room2);
-    setAdultPop2(storage.AdultPop2);
-    setChildPop2(storage.ChildPop2);
-    setRoom3(storage.Room3);
-    setAdultPop3(storage.AdultPop3);
-    setChildPop3(storage.ChildPop3);
-    setRoom4(storage.Room4);
-    setAdultPop4(storage.AdultPop4);
-    setChildPop4(storage.ChildPop4);
+    let storage = JSON.parse(localStorage.getItem("RoomsReducer"));
+    console.log("storage", storage);
+    dispatchPopulateData(storage);
   };
-  */
+
+  const dispatchPopulateData = data => {
+    dispatch({
+      type: "POPULATE_DATA",
+      data: data
+    });
+  };
 
   //check localstorage for Rooms object
   useEffect(() => {
-    dispatch({
-      type: "POPULATE_DATA",
-      data: InitValues
-    });
-    /*
+    dispatchPopulateData(InitValues);
+
     try {
-      if (localStorage.getItem("Rooms")) {
+      if (localStorage.getItem("RoomsReducer")) {
         setStateFromStorage();
       }
     } catch (e) {}
-    */
   }, []);
 
   //console.log("dataReducer", dataReducer);
@@ -84,17 +47,6 @@ const MainReducer = () => {
     <DataContext.Provider value={{ dataReducer, dispatch }}>
       <form onSubmit={formSubmit}>
         <fieldset style={fieldset}>
-          {/*roomData &&
-            roomData.map((data, index) => {
-              return (
-                <RoomboxReducer
-                  key={index + data.name}
-                  params={data}
-                  checkNeighborRoom={checkNeighborRoom}
-                />
-              );
-            })
-        */}
           {dataReducer &&
             Object.keys(dataReducer).map((data, index) => {
               return <RoomboxReducer key={index + data} room={data} />;
