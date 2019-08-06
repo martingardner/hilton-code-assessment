48fb97b039f524d3fd8338e5197966ab97f5a854
martin gardner II <martingardnerii@gmail.com>
2019-08-06 00:45:21 -0500

    update childpop-reducer tests

--- tests/childpop-reducer.test.js
+++ tests/childpop-reducer.test.js
@@ -1,51 +1,52 @@
 import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
-import AdultpopReducer from "../components/adultpop-reducer";
+import ChildpopReducer from "../components/childpop-reducer";
 
 import MainReducer from "../pages/index-reducer";
 import DataContext from "../context/DataContext";
 
 afterEach(cleanup);
 
-describe("Adultpop component", () => {
-  it("Adultpop should exist", () => {
+describe("Childpop Component", () => {
+  it("childpop should exist", () => {
     const component = render(
       <MainReducer>
         <DataContext.Provider>
-          <AdultpopReducer room={1} />
+          <ChildpopReducer room={1} />
         </DataContext.Provider>
       </MainReducer>
     );
+
     expect(component).toBeTruthy();
   });
 
-  it("Adultpop select inital value should equal '1'", () => {
+  it("childpop select inital value should equal '0'", () => {
     const component = render(
       <MainReducer>
         <DataContext.Provider>
-          <AdultpopReducer room={1} />
+          <ChildpopReducer room={1} />
         </DataContext.Provider>
       </MainReducer>
     );
-    expect(document.querySelector("[name='room1_adultpop']").value).toEqual(
-      "1"
+    expect(document.querySelector("[name='room1_childpop']").value).toEqual(
+      "0"
     );
   });
 
-  it("Adultpop should have a value of 2 when set to 2 - test of dispatch", () => {
+  it("childpop should have a value of 2 when set to 2 - test of dispatch", () => {
     const component = render(
       <MainReducer>
         <DataContext.Provider>
-          <AdultpopReducer room={1} />
+          <ChildpopReducer room={1} />
         </DataContext.Provider>
       </MainReducer>
     );
 
-    fireEvent.change(document.querySelector("[name='room1_adultpop']"), {
+    fireEvent.change(document.querySelector("[name='room1_childpop']"), {
       target: { value: "2" }
     });
 
-    expect(document.querySelector("[name='room1_adultpop']").value).toEqual(
+    expect(document.querySelector("[name='room1_childpop']").value).toEqual(
       "2"
     );
   });
